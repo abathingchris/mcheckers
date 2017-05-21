eb4871dbe5c6726f8557b8274ef48f0051ac35e2
melnaise77 <BGRemodeling@gmail.com>
2017-05-21 12:54:40 -0700

    initial commit

---
+++ mcheckers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mcheckers
+//
+//  Created by Melissa Golson on 5/21/17.
+//  Copyright © 2017 bgsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
