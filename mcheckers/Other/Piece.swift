596752976ba0b30ffd7dcf80d66b93c7c18f65fe
Chris Long <chris.long@mac.com>
2017-06-03 11:40:41 -0700

    updated path to base files

---
+++ mcheckers/Other/Piece.swift
@@ -0,0 +1,16 @@
+//
+//  Piece.swift
+//  mcheckers
+//
+//  Created by Melissa Golson on 6/3/17.
+//  Copyright © 2017 bgsoft. All rights reserved.
+//
+
+import Foundation
+
+class Piece {
+    var owner = "p1"
+    var location = "A2"
+    var kinged = false
+    var score = 2
+}
---
+++ mcheckers/Other/Square.swift
@@ -0,0 +1,16 @@
+//
+//  Square.swift
+//  mcheckers
+//
+//  Created by Melissa Golson on 6/2/17.
+//  Copyright © 2017 bgsoft. All rights reserved.
+//
+
+import Foundation
+
+class Square {
+    var color = "white"
+    var location = "A1"
+    var occupied = true
+}
+
